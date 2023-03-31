687cd65491584337324474383eb5bc18e74685e0
RiyaaVishnoi <107324623+RiyaaVishnoi@users.noreply.github.com>
2023-03-31 14:52:56 +1300

    Update checkout.js

--- Assets/JS/checkout.js
+++ Assets/JS/checkout.js
@@ -5,7 +5,7 @@ function submitform() {
 const cartData = JSON.parse(localStorage.getItem("cart"));
 const cartItemsTable = document.getElementById("cartItemsTable");
 let cartTotal = 0;
-
+//for each item in the cart create a tr and td elemets so that we can display them according to our style
 if (cartData && cartData.length > 0) {
   cartData.forEach((item) => {
     const tr = document.createElement("tr");
@@ -21,7 +21,7 @@ if (cartData && cartData.length > 0) {
     const tdQuantity = document.createElement("td");
     tdQuantity.innerText = item.quantity;
     tr.appendChild(tdQuantity);
-
+//adds the whole row to the end of the table
     cartItemsTable.appendChild(tr);
 
     cartTotal += parseFloat(item.price) * item.quantity; // Calculate total price for each item
